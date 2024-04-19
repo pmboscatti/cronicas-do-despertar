f37896b8d2b8700847e340b0e1e10432c639b646
Pedro Boscatti <pedroboscatti@gmail.com>
2024-04-18 22:34:05 -0300

    correção do movimento do inimigo

--- Assets/Scripts/SimpleNPCMovement.cs
+++ Assets/Scripts/SimpleNPCMovement.cs
@@ -5,7 +5,15 @@ public class SimpleNPCMovement : MonoBehaviour
     public float speed = 2f; // Velocidade de movimento do NPC
     public Transform[] waypoints; // Pontos ao longo dos quais o NPC irá se mover
     private int currentWaypointIndex = 0; // Índice do ponto atual
+
+    Rigidbody2D rbody;
     IsometricCharacterRenderer isoRenderer;
+
+    private void Awake()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
+    }
 
     void Update()
     {
@@ -13,15 +21,15 @@ public class SimpleNPCMovement : MonoBehaviour
         if (waypoints.Length > 0)
         {
             // Calcula a direção para o próximo ponto
-            Vector3 direction = waypoints[currentWaypointIndex].position - transform.position;
-            direction.Normalize();
+            Vector2 direction = ((Vector2)waypoints[currentWaypointIndex].position - (Vector2)transform.position).normalized;
 
             // Move o NPC na direção do próximo ponto
+            Vector2 movement = direction * speed;
+            isoRenderer.SetDirection(movement);
             transform.Translate(direction * speed * Time.deltaTime);
 
-
             // Verifica se o NPC chegou ao ponto atual
-            if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].position) < 0.1f)
+            if (Vector2.Distance(transform.position, waypoints[currentWaypointIndex].position) < 0.1f)
             {
                 // Se sim, avança para o próximo ponto
                 currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
