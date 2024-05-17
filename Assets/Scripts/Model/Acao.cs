c963cafa80f2787574f7c00c397ac937417a997f
Pedro Boscatti <pedroboscatti@gmail.com>
2024-05-16 21:02:01 -0300

    adição de inimigos no primeiro cenário

--- Assets/Scripts/Model/Acao.cs
+++
@@ -1,9 +0,0 @@
-class Acao{
-    public string nome;
-    public int pp;//powerpoint
-    public int precisao;
-    public bool erraAcao()
-    {
-        return RNJesus.erraAcao(precisao);
-    }
-}
