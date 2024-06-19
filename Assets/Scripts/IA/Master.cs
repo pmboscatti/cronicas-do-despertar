e3b61cdc251bd0a08539842c4787f9b090176f04
Allan Gomes <113448955+R4msas@users.noreply.github.com>
2024-06-19 13:32:11 -0300

    Arquivo master para debug da IA, MinHeap para funcionamento do Dijkstra

--- Assets/Scripts/IA/Master.cs
+++ Assets/Scripts/IA/Master.cs
@@ -1,35 +1,38 @@
 using System;
 using Assets.Scripts.Model;
-
-public class Master
+namespace Assets.Scripts.IA
 {
-    public static void Main(string[] args)
+    public class Master
     {
-        Personagem cleitinho = new("Cleitinho", 200, 100, 100, 100, 100, 100);
-        Personagem felisberto = new("Felisberto", 200, 160, 120, 110, 130, 101);
-        Inimigo fulano = new("Fulano", 200, 100, 100, 100, 100, 100, 5, 8);
-        Inimigo beltrano = new("Beltrano", 200, 115, 100, 100, 100, 100, 8, 5);
-        Ataque[] ataques = new Ataque[2];
-        Magia[] magias = new Magia[2];
-        ataques[0] = new Ataque("ataque de espada", 8, 90, 50);
-        ataques[1] = new Ataque("ataque de espada forte", 8, 80, 70);
-        magias[0] = new Magia("Lança chamas", 8, 90, 50);
-        magias[1] = new Magia("Bola de fogo", 8, 80, 70);
-        fulano.ataques = ataques;
-        fulano.magias = magias;
-        beltrano.magias = magias;
-        beltrano.ataques = ataques;
-        cleitinho.ataques = ataques;
-        cleitinho.magias = magias;
-        felisberto.ataques = ataques;
-        felisberto.magias = magias;
-        GrafoDecisao grafo = new(fulano);
-        grafo.CriarGrafo();
-        Acao acao = grafo.Dijkstra();
-        Console.WriteLine(acao.nome + acao.GetAlvo());
-        grafo = new(beltrano);
-        grafo.CriarGrafo();
-        acao = grafo.Dijkstra();
-        Console.WriteLine(acao.nome + acao.GetAlvo());
+        public static void Main(string[] args)
+        {
+            Personagem cleitinho = new("Cleitinho", 200, 100, 100, 100, 100, 100);
+            Personagem felisberto = new("Felisberto", 200, 160, 120, 110, 130, 101);
+            Inimigo fulano = new("Fulano", 200, 100, 100, 100, 100, 100, 5, 8);
+            Inimigo beltrano = new("Beltrano", 200, 115, 100, 100, 100, 100, 8, 5);
+            Ataque[] ataques = new Ataque[2];
+            Magia[] magias = new Magia[2];
+            ataques[0] = new Ataque("ataque de espada", 8, 90, 50);
+            ataques[1] = new Ataque("ataque de espada forte", 8, 80, 70);
+            magias[0] = new Magia("Lança chamas", 8, 90, 50);
+            magias[1] = new Magia("Bola de fogo", 8, 80, 70);
+            fulano.ataques = ataques;
+            fulano.magias = magias;
+            beltrano.magias = magias;
+            beltrano.ataques = ataques;
+            cleitinho.ataques = ataques;
+            cleitinho.magias = magias;
+            felisberto.ataques = ataques;
+            felisberto.magias = magias;
+            GrafoDecisao grafo = new(fulano);
+            grafo.CriarGrafo();
+            Acao acao = grafo.Dijkstra();
+            Console.WriteLine(acao.nome + acao.GetAlvo());
+            grafo = new(beltrano);
+            grafo.CriarGrafo();
+            acao = grafo.Dijkstra();
+            Console.WriteLine(acao.nome + acao.GetAlvo());
+        }
     }
+
 }
