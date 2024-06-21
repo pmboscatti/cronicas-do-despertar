6c92df327b54dd5515cc5cd399d4f01a7ee10012
Allan Gomes <113448955+R4msas@users.noreply.github.com>
2024-06-20 22:34:51 -0300

    Pequenas correções no estado de batalha, aguardando a sincronização com a FSM

--- Assets/Scripts/Battle/BattleState.cs
+++ Assets/Scripts/Battle/BattleState.cs
@@ -44,7 +44,8 @@ namespace Assets.Scripts.Battle
 			}
 			if (fimDaBatalha == false)
 			{
-				//chamar o próximo estado;
+				//chamada para o próximo estado de batalha
+				EstadoEscolha();
 			}
 		}
 
@@ -89,8 +90,7 @@ namespace Assets.Scripts.Battle
 	public List<Acao> DecisaoJogador()
 	{
 		List<Acao> listaDeAcoes=new List<Acao>();
-
-		//receber a lista de ações de escolha do jogador;
+		//listaDeAcoes= () //receber a lista de ações vinda do método do Pedro
 		return listaDeAcoes;
 	}
 	public void EscolhaDeAcoes()
@@ -98,9 +98,84 @@ namespace Assets.Scripts.Battle
 		List<Acao> listaDeAcoes=DecisaoJogador();
 		DecisaoIA(listaDeAcoes);
 		MaxHeap heap=CriaMaxHeap(listaDeAcoes);
+		ExecutaAcoes(heap);//aqui já é a chamada para o próximo estado
+	}
+	public void IniciaAliados()
+	{
+		aliados=new();
+		Personagem Guerreiro = new("Guerreiro", 200, 150, 150, 50, 50, 50);
+		Personagem Mago = new("Mago", 200, 80,20 , 180, 130, 101);
+		Ataque[] ataques = new Ataque[2];
+		Magia[] magias = new Magia[2];
+		Cura[]curas=new Cura[2];
+		ataques[0] = new Ataque("ataque de espada", 8, 90, 50);
+		ataques[1] = new Ataque("ataque de machado", 8, 80, 70);
+		magias[0] = new Magia("Lança chamas", 8, 90, 50);
+		magias[1] = new Magia("Bola de fogo", 8, 80, 70);
+		curas[0] = new Cura("Recuperar",5,90,50);
+		curas[1] = new Cura("Benção do Deus do Sol ",5,70,100);
+		Guerreiro.ataques = ataques;
+		Guerreiro.magias = magias;
+		Mago.ataques = ataques;
+		Mago.magias = magias;
+		Status[]status=new Status[6];
+		status[0]= new Status("Maldição de lentidão",5, 70,Stat.velocidade, -2);
+		status[1]= new Status("Intimidar",5, 90,Stat.atk, -1);
+		status[2]=  new Status("Benção de força",5, 90,Stat.atk, 1);
+		status[3]=  new Status("Pele de pedra",5, 90,Stat.def, 1);
+		status[4]=  new Status("Benção de Hermes",5, 90,Stat.velocidade, 1);
+		status[5]=  new Status("Armadura mágica",5, 90,Stat.spdef, 1);
+		Mago.status=status;
+		aliados.Add(Guerreiro);
+		aliados.Add(Mago);
+	}
+	public void IniciaInimigos()
+	{
+		inimigos=new();
+		Inimigo Tonhao = new("Tonhão",  100, 100, 100, 100, 100, 100, 5, 8);
+		Inimigo Tiao= new("Tião",200, 115, 100, 100, 100, 100, 8, 5);
+		Ataque[] ataques = new Ataque[2];
+		Magia[] magias = new Magia[2];
+		ataques[0] = new Ataque("Soco", 8, 90, 50);
+		ataques[1] = new Ataque("Pedrada", 8, 80, 70);
+		magias[0] = new Magia("Lança Chamas", 8, 90, 50);
+		magias[1] = new Magia("Bola de fogo", 8, 80, 70);
+		Tonhao.ataques=ataques;
+		Tonhao.magias=magias;
+		Tiao.ataques=ataques;
+		Tiao.magias=magias;
+		inimigos.Add(Tonhao);
+		inimigos.Add(Tiao);
+
+	}
+	
+//para chamar no estado de inicialização
+	public void Inicialização()
+	{
+		IniciaAliados();
+		IniciaInimigos();
+
+	}
+	//estado de escolha
+	public void EstadoEscolha()
+	{
+		EscolhaDeAcoes();
+	}
+	//estado de execução
+	public void ExecutaAcoes(MaxHeap heap)
+	{
 		EfetuaAtaques(heap);
+
 	}
+	//estado de fim de turno
+	public void FimDeTurno()
+	{
+		AtualizaListaDeVivos();
 	}
+	
+	
+	}
+
 	
 	
 }
